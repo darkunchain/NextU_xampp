e89b68ebafacf1bb59b2003ef1e3b38ab470c3bb
darkunchain <luisfer74081@gmail.com>
2020-01-23 16:27:17 -0500

    hoy

--- blogs/food/js/app.js
+++ blogs/food/js/app.js
@@ -1,1 +1,11 @@
-$(document).foundation()
+var buscar = document.getElementsByClassName('tituloarticulo');
+var fecha = document.getElementsByClassName('fecha');
+console.log('buscar1: ',buscar.length,'  fecha1: ', fecha.length) 
+var i;
+for (i = 0; i < buscar.length; i++) { 
+  console.log('buscar2: ',buscar[i].innerHTML,'  fecha2: ', fecha[i].innerHTML)  
+  var list = "lista"+i
+  var fech = "fecha"+i  
+  document.getElementById(list).innerHTML = buscar[i].innerHTML
+  document.getElementById(fech).innerHTML = fecha[i].innerHTML
+}

bf38d5d3e69dcc30ff1127c7421b12d4f8f51a12
darkunchain <luisfer74081@gmail.com>
2020-02-03 18:43:25 -0500

    hoy

--- blogs/travel/js/app.js
+++ blogs/travel/js/app.js
@@ -3,6 +3,24 @@ document.addEventListener('DOMContentLoaded', function() {
     var instances = M.FloatingActionButton.init(elems, {
       direction: 'bottom'
     });
+
+    var dropdelem = document.querySelectorAll('.dropdown-trigger');
+    var instances = M.Dropdown.init(dropdelem, {
+      alignment: 'right',
+      closeOnClick: 'true'     
+    });
+
+    var paraelems = document.querySelectorAll('.parallax');
+    var instances = M.Parallax.init(paraelems, {
+      responsiveThreshold: 0
+    });
+    
+    
+  
+
+
   });
+
+  
 
   
